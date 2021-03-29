500f6b53a2dff41e1a9ed280ca567fe1db1017c0
Kallum Jones <kallummj@hotmail.com>
2021-03-29 16:52:28 +0100

    - Added plugin info page - Added Rules/Whitelist page

---
+++ static/scripts/components/tooltip.js
@@ -0,0 +1,32 @@
+function copied(ip) {
+    // Create new element
+    const el = document.createElement('textarea')
+
+    // Set value (string to be copied)
+    el.value = ip
+
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '')
+    el.style = { position: 'absolute', left: '-9999px' }
+    document.body.appendChild(el)
+
+    // Select text inside element
+    el.select()
+
+    // Copy text to clipboard
+    document.execCommand('copy')
+
+    // Remove temporary element
+    document.body.removeChild(el)
+
+    // Set the tooltip text
+    this.tooltipText = 'Copied!'
+
+    const tooltip = document.querySelector("[data-tooltip]")
+    tooltip.setAttribute("data-tooltip", "Copied!")
+
+    // Reset tooltip
+    setTimeout(() => {
+      tooltip.setAttribute("data-tooltip", "Click to Copy!")
+    }, 5000)
+  }
