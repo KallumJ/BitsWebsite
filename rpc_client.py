04bc4da0d8200335d04af36879d5a7d4370e1fb3
Nex <nex@bits.team>
2021-10-03 12:49:03 +0200

    Attempt to fix issue #13 by not keeping the consumer open and by reconnecting when possible (#27)
    Fixed a typo
    
    Attempt to fix issue #13 by not keeping the consumer open and by reconnecting when possible
    
    Reviewed-on: https://hogwarts.bits.team/git/Bits/BitsWebsite/pulls/27
    Reviewed-by: Kallum <kallummj071001@gmail.com>

--- rpc_client.py
+++ rpc_client.py
@@ -1,15 +1,33 @@
 import pika
+from pika.exceptions import AMQPConnectionError
+
 from config import rabbitMQPassword, rabbitMQUsername, rabbitMQLocalHostname
 from remote_server_utils import check_on_hogwarts
 import uuid
 
+# I changed the way this client works, so it's now slightly different than the
+# example. Instead of keeping our consumer open at all times, we close it as soon
+# as we've received the reply from the server, and start consuming again the next
+# time we want to send a query. I also added some extra checks to make sure we
+# reconnect if the connection is ever lost. - Nex
+
 
 class RPCClient:
     def __init__(self, hostname, exchange):
+        self.exchange = exchange
+        self.hostname = hostname
+        self.connection = None
+        self.connect()
+
+    def connect(self):
+        # if the connection is already open, don't do anything
+        if self.connection is not None and self.connection.is_open:
+            return
+
         if check_on_hogwarts():
             credentials = pika.PlainCredentials(rabbitMQUsername, rabbitMQPassword)
             self.connection = pika.BlockingConnection(
-                pika.ConnectionParameters(host=hostname, credentials=credentials))
+                pika.ConnectionParameters(host=self.hostname, credentials=credentials))
         else:
             self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=rabbitMQLocalHostname))
 
@@ -18,17 +36,12 @@ class RPCClient:
         result = self.channel.queue_declare(queue="", exclusive=True)
         self.callback_queue = result.method.queue
 
-        self.channel.basic_consume(
-            queue=self.callback_queue,
-            on_message_callback=self.on_response,
-            auto_ack=True
-        )
-
-        self.exchange = exchange
-
     def on_response(self, ch, method, props, body):
         if self.correlation_id == props.correlation_id:
+            # store the response
             self.response = body
+            # stop consuming on the reply queue
+            self.channel.basic_cancel(self.consumer_tag)
 
     """
     A method to make calls to the API with GraphQL
@@ -40,17 +53,40 @@ class RPCClient:
     }
     """
     def call(self, payload):
-        self.response = None
-        self.correlation_id = str(uuid.uuid4())
-        self.channel.basic_publish(
-            exchange=self.exchange,
-            routing_key="",
-            properties=pika.BasicProperties(
-                reply_to=self.callback_queue,
-                correlation_id=self.correlation_id
-            ),
-            body=payload
-        )
-        while self.response is None:
-            self.connection.process_data_events()
-        return self.response
+        # if we don't have a connection, open a connection first
+        if not self.connection.is_open:
+            print("Connection lost, reconnecting")
+            self.connect()
+
+        try:
+            # clear the response and generate a new correlation ID
+            self.response = None
+            self.correlation_id = str(uuid.uuid4())
+
+            # we start consuming on our reply queue
+            self.consumer_tag = self.channel.basic_consume(
+                queue=self.callback_queue,
+                on_message_callback=self.on_response,
+                auto_ack=True
+            )
+
+            # publish the query to the server
+            self.channel.basic_publish(
+                exchange=self.exchange,
+                routing_key="",
+                properties=pika.BasicProperties(
+                    reply_to=self.callback_queue,
+                    correlation_id=self.correlation_id
+                ),
+                body=payload
+            )
+
+            # wait for a response
+            while self.response is None:
+                self.connection.process_data_events()
+            return self.response
+
+        except AMQPConnectionError:
+            print("Connection lost, reconnecting")
+            self.connect()
+            return self.call(payload)
